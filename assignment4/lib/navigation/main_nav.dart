6b17490c4867d944d30f7189aa9f3a88d190f8c7
Leebonggu <leebonggu12@hanmail.net>
2023-12-10 02:16:36 +0900

    init files

---
+++ assignment4/lib/navigation/main_nav.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MainNav extends StatefulWidget {
+  const MainNav({super.key});
+
+  @override
+  State<MainNav> createState() => _MainNavState();
+}
+
+class _MainNavState extends State<MainNav> {
+  final int _selectedIndex = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
